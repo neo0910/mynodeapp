f3610ca674b70e4f228aa489989230c1b0b1cfba
neo0910 <denikkalenik@gmail.com>
2021-02-16 22:34:27 +0300

    router training

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const express = require('express')
-const path = require('path')
 const cookieParser = require('cookie-parser')
+const express = require('express')
 const logger = require('morgan')
+const path = require('path')
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
+const birds = require('./routes/birds')
 
 const app = express()
 
@@ -16,5 +17,29 @@ app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
+app.use('/birds', birds)
+
+app.get('/example/b', function (req, res, next) {
+  console.log('the response will be sent by the next function ...')
+  next()
+}, function (req, res) {
+  res.send('Hello from B!')
+})
+
+const cb0 = function (req, res, next) {
+  console.log('CB0')
+  next()
+}
+
+const cb1 = function (req, res, next) {
+  console.log('CB1')
+  next()
+}
+
+const cb2 = function (req, res) {
+  res.send('Hello from C!')
+}
+
+app.get('/example/c', [cb0, cb1, cb2])
 
 module.exports = app
