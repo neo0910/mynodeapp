a7ebc2c90add9d27d244d9c03cef656f3947df93
neo0910 <denikkalenik@gmail.com>
2021-02-21 18:59:05 +0300

    start work with graphql

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const logger = require('morgan')
 const path = require('path')
+const graphqlHTTP = require('express-graphql').graphqlHTTP
 
 const indexRouter = require('./routes/index')
-const usersRouter = require('./routes/users')
-const birds = require('./routes/birds')
+const schema = require('./schema/schema')
 
 const app = express()
 
@@ -13,33 +13,17 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
+app.use('/graphql', graphqlHTTP({
+  schema,
+  graphiql: true
+}))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
-app.use('/birds', birds)
 
-app.get('/example/b', function (req, res, next) {
-  console.log('the response will be sent by the next function ...')
-  next()
-}, function (req, res) {
-  res.send('Hello from B!')
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
 })
-
-const cb0 = function (req, res, next) {
-  console.log('CB0')
-  next()
-}
-
-const cb1 = function (req, res, next) {
-  console.log('CB1')
-  next()
-}
-
-const cb2 = function (req, res) {
-  res.send('Hello from C!')
-}
-
-app.get('/example/c', [cb0, cb1, cb2])
 
 module.exports = app
