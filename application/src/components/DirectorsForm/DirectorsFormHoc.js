59db5bdd18e2b99c4e186e63e23a557aa13e1a9f
neo0910 <denikkalenik@gmail.com>
2021-02-27 23:38:08 +0300

    added create mutation to fe

--- application/src/components/DirectorsForm/DirectorsFormHoc.js
+++ application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -1,6 +1,18 @@
 import { withStyles } from '@material-ui/core/styles';
 import { compose } from 'recompose';
+import {graphql} from 'react-apollo';
 
 import { styles } from './styles';
+import {addDirectorMutation} from './mutations';
+import {directorsQuery} from '../DirectorsTable/queries';
 
-export default compose(withStyles(styles));
+const withGraphqlAdd = graphql(addDirectorMutation, {
+    props: ({mutate}) => ({
+        addDirector: director => mutate({
+            variables: director,
+            refetchQueries: [{query: directorsQuery}]
+        })
+    })
+});
+
+export default compose(withStyles(styles), withGraphqlAdd);
