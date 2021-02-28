d4c98f520131e57b66999787bf93c67803a875d9
neo0910 <denikkalenik@gmail.com>
2021-02-28 11:55:06 +0300

    added update mutations to fe

--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -3,17 +3,27 @@ import { compose } from 'recompose';
 import {graphql} from 'react-apollo';
 
 import { styles } from './styles';
-import {addMovieMutation} from './mutations';
+import {addMovieMutation, updateMovieMutation} from './mutations';
 import {moviesQuery} from '../MoviesTable/queries';
 import {directorsQuery} from './queries';
 
-const withGraphqlAdd = graphql(addMovieMutation, {
-    props: ({mutate}) => ({
-        addMovie: movie => mutate({
-            variables: movie,
-            refetchQueries: [{query: moviesQuery}]
+const withGraqhQL = compose(
+    graphql(addMovieMutation, {
+        props: ({mutate}) => ({
+            addMovie: movie => mutate({
+                variables: movie,
+                refetchQueries: [{query: moviesQuery}]
+            })
         })
-    })
-});
+    }),
+    graphql(updateMovieMutation, {
+        props: ({mutate}) => ({
+            updateMovie: movie => mutate({
+                variables: movie,
+                refetchQueries: [{query: moviesQuery}]
+            })
+        })
+    }),
+);
 
-export default compose(withStyles(styles), withGraphqlAdd, graphql(directorsQuery));
+export default compose(withStyles(styles), withGraqhQL, graphql(directorsQuery));
--- application/src/components/MoviesForm/mutations.js
+++ application/src/components/MoviesForm/mutations.js
@@ -7,3 +7,11 @@ export const addMovieMutation = gql`
         }
     }
 `;
+
+export const updateMovieMutation = gql`
+    mutation updateMovie($id: ID, $name: String!, $genre: String!, $rate: Int, $watched: Boolean!, $directorId: ID) {
+        updateMovie(id: $id, name: $name, genre: $genre, rate: $rate, watched: $watched, directorId: $directorId) {
+            name
+        }
+    }
+`;
